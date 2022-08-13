bd0f7e9b3651a6df80a209f52105d58a457a05b1
StevendeJong🍟 <steven@steven-dejong.nl>
2022-08-13 16:52:23 +0200

    Check for connected displays

--- dot/config/bspwm/monitor-setup.sh
+++ dot/config/bspwm/monitor-setup.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-if [[ $(xrandr -q | grep 'HDMI-1-2 connected') ]]; then
-	## Set gpu and monitor
-	xrandr --setprovideroutputsource 1 0
-
-	### Max card resolution (LG Monitor)
-	cvt 2048 864 60
-	xrandr --newmode "2048x864_60.00"  145.25  2048 2168 2376 2704  864 867 877 897 -hsync +vsync
-	xrandr --addmode HDMI-1-2 2048x864_60.00
-
-	autorandr --load triple
-	
-	sleep 3
-
-	bspc monitor HDMI-1-2 -d 1 2 3
-	bspc monitor HDMI-1 -d 4 5 6
-	bspc monitor DP-1 -d 7 8 9
-else
-	autorandr --load double
-	
-	sleep 3
-
-	bspc monitor HDMI-1 -d 1 2 3 4 5
-	bspc monitor DP-1 -d 6 7 8 9
+DISPLAYPORT = 'DP-1'
+HDMIONE = 'HDMI-1'
+HDMITWO = 'HDMI-1-1'
+if [[ $(xrandr -q | grep 'DP-2 connected') ]]; then
+	DISPLAYPORT = 'DP-2'
 fi
+
+if [[ $(xrandr -q | grep 'HDMI-2 connected') ]]; then
+	HDMIONE = 'HDMI-2'
+fi
+
+if [[ $(xrandr -q | grep 'HDMI-1-2 connected') ]]; then
+	HDMITWO = 'HDMI-1-2'
+fi
+
+## Set gpu and monitor
+xrandr --setprovideroutputsource 1 0
+
+### Max card resolution (LG Monitor)
+cvt 2048 864 60
+xrandr --newmode "2048x864_60.00"  145.25  2048 2168 2376 2704  864 867 877 897 -hsync +vsync
+xrandr --addmode $HDMITWO 2048x864_60.00
+
+autorandr --load triple
+
+sleep 3
+
+bspc monitor $HDMITWO -d 1 2 3
+bspc monitor $HDMIONE -d 4 5 6
+bspc monitor $DISPLAYPORT -d 7 8 9
 
 notify-send -i display "Display profile" "$(autorandr --current)"
