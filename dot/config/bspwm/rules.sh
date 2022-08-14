c64aaef4b446d41705d2f455e8b8e3d95e93b874
StevendeJong🍟 <steven@steven-dejong.nl>
2022-08-14 23:20:13 +0200

    Add rules

---
+++ dot/config/bspwm/rules.sh
@@ -0,0 +1,2 @@
+bspc rule -a whatsapp-nativefier -o desktop=^1 follow=on
+bspc rule -a telegram-desktop -o desktop=^1 follow=off
--- dot/config/bspwm/styling.sh
+++ dot/config/bspwm/styling.sh
@@ -1,6 +1,9 @@
-bspc config border_width         1
-bspc config border_radius        2
-bspc config window_gap           2
+bspc config border_width         2
+bspc config border_radius        4
+bspc config window_gap           4
+
+bspc config normal_border_color "#2E3440"
+bspc config focused_border_color "#D8DEE9"
 
 bspc config split_ratio          0.52
 bspc config borderless_monocle   true
