f6b0f56b55c75716e897ba727025e7aa805c49a1
StevendeJong🍟 <steven@steven-dejong.nl>
2022-08-14 22:11:57 +0200

    Update monitor setup

--- dot/config/bspwm/monitor-setup.sh
+++ dot/config/bspwm/monitor-setup.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-DISPLAYPORT='DP-1'
-HDMIONE='HDMI-1'
-HDMITWO='HDMI-1-1'
-if [[ $(xrandr -q | grep 'DP-2 connected') ]]; then
-	DISPLAYPORT='DP-2'
+LG_HDMI='HDMI-1-1'
+AOC_HDMI='HDMI-1'
+AOC_DP='DP-1'
+
+if [[ $(xrandr -q | grep 'HDMI-1-2 connected') ]]; then
+	LG_HDMI='HDMI-1-2'
 fi
 
 if [[ $(xrandr -q | grep 'HDMI-2 connected') ]]; then
-	HDMIONE='HDMI-2'
-fi
-
-if [[ $(xrandr -q | grep 'HDMI-1-2 connected') ]]; then
-	HDMITWO='HDMI-1-2'
+	AOC_HDMI='HDMI-2'
 fi
 
 ## Set gpu and monitor
@@ -21,17 +18,15 @@ xrandr --setprovideroutputsource 1 0
 ### Max card resolution (LG Monitor)
 cvt 2048 864 60
 xrandr --newmode "2048x864_60.00"  145.25  2048 2168 2376 2704  864 867 877 897 -hsync +vsync
-xrandr --addmode $HDMITWO 2048x864_60.00
+xrandr --addmode $LG_HDMI 2048x864_60.00
 
-autorandr --load triple
+autorandr --load $LG_HDMI
 
-xrandr --output $HDMIONE --mode 1920x1080 --rate 144
-xrandr --output $DISPLAYPORT --mode 1920x1080 --rate 144
+# xrandr --output $HDMIONE --mode 1920x1080 --rate 144
+# xrandr --output $DISPLAYPORT --mode 1920x1080 --rate 144
 
 sleep 3
 
-bspc monitor $HDMITWO -d 1 2 3
-bspc monitor $HDMIONE -d 4 5 6
-bspc monitor $DISPLAYPORT -d 7 8 9
-
-notify-send -i display "Display profile" "$(autorandr --current)"
+bspc monitor $LG_HDMI -d 1 2 3
+bspc monitor $AOC_HDMI -d 4 5 6
+bspc monitor $AOC_DP -d 7 8 9
