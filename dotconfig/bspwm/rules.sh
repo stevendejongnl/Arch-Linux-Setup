45f80e027614feaf0bd200c32524143b2367d1de
StevendeJong🍟 <steven@steven-dejong.nl>
2022-08-17 00:38:00 +0200

    Update rules and hotkey help tools

--- dotconfig/bspwm/rules.sh
+++ dotconfig/bspwm/rules.sh
@@ -3,6 +3,22 @@
 # The bspc rule -a command only uses XDG class names, which can be easily found out by using xprop:
 # xprop | grep WM_CLASS
 
+# Rules explained
+# -o|--one-shot [Run rule once]
+
+# States explained
+# tiled
+#   Its size and position are determined by the window tree.
+#
+# pseudo_tiled
+#   A tiled window that automatically shrinks but doesn’t stretch beyond its floating size.
+#
+# floating
+#   Can be moved/resized freely. Although it doesn’t use any tiling space, it is still part of the window tree.
+#
+# fullscreen
+#   Fills its monitor rectangle and has no borders.
+
 bspc config ignore_ewmh_focus true
 
 # https://github.com/phenax/bsp-layout
@@ -10,9 +26,9 @@ bsp-layout set even 1
 bsp-layout set even 2
 bsp-layout set even 3
 
-# bspc desktop -f ^1
 
-# bspc rule -a whatsapp-nativefier-d40211 desktop=^1 follow=off
-# bspc rule -a TelegramDesktop desktop=^1 follow=off
-
-bspc desktop -f ^4
+bspc rule -a whatsapp-nativefier-d40211 follow=off hidden
+bspc rule -a TelegramDesktop follow=off hidden
+bspc rule -a Timer-for-harvest follow=off state=floating
+bspc rule -a Pavucontrol follow=off state=floating
+bspc rule -a Blueman-manager follow=off state=floating
