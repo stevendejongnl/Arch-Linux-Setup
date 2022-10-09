a926c02a35c9b9c9b32644608d5691dd2ae53a01
StevendeJong🍟 <steven@steven-dejong.nl>
2022-10-09 22:38:27 +0200

    Blurred background xsecurelock

---
+++ setup/xsecurelock.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+install () {
+    mkdir -p $HOME/.config/dunst
+    rsync -av --progress "$DIR/dotconfig/xsecurelock/" "$HOME/.config/xsecurelock/"
+}
+
+if [ -n "$INSTALL_ALL" ]; then
+    install
+else
+    read -r -p "Install xsecurelock config [y/N] " install_question
+    if [[ "$install_question" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+        install
+    fi
+fi
