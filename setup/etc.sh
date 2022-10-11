fb13a4b83f96862ffebae1d74a41a84fc4cc26ef
StevendeJong🍟 <steven@steven-dejong.nl>
2022-10-11 09:43:44 +0200

    Polybar package update hook

--- setup/etc.sh
+++ setup/etc.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 
 install () {
-    sudo rsync -av --progress "$DIR/etc/profile.d/" "/etc/profile.d/"
+    # sudo rsync -av --progress "$DIR/etc/profile.d/" "/etc/profile.d/"
+    sudo rsync -av --progress "$DIR/etc/pacman.d/" "/etc/pacman.d/"
 }
 
 if [ -n "$INSTALL_ALL" ]; then
     install
 else
-    read -r -p "Install etc profile.d files [y/N] " install_question
+    read -r -p "Install etc files [y/N] " install_question
     if [[ "$install_question" =~ ^([yY][eE][sS]|[yY])$ ]]; then
         install
     fi
