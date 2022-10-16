3028bddf2be6ade83ca3e1b1055dd685ba1fd660
StevendeJong🍟 <steven@steven-dejong.nl>
2022-10-16 19:32:45 +0200

    Blurred background xsecurelock

--- god-mode/dotconfig/xsecurelock/blurred-background.sh
+++ god-mode/dotconfig/xsecurelock/blurred-background.sh
@@ -10,7 +10,8 @@ export XSECURELOCK_IMAGE_DURATION_SECONDS=3600
 
 if [ -z ${LOCKSCREEN_DIR+x} ]; then exit 1; fi # bail if lockscreen dir is not set
 
-scrot ${LOCKSCREEN_DIR}/lockscreen-plain.png --overwrite && 
+/usr/bin/rm -f ${LOCKSCREEN_DIR}/lockscreen-plain.png
+flameshot screen --path ${LOCKSCREEN_DIR}/lockscreen-plain.png && 
 convert ${LOCKSCREEN_DIR}/lockscreen-plain.png -blur $BLURTYPE ${LOCKSCREEN_DIR}/lockscreen-blurred.png &&
 mv ${LOCKSCREEN_DIR}/lockscreen-blurred.png ${LOCKSCREEN_DIR}/lockscreen/lockscreen.png &
 xsecurelock &&
